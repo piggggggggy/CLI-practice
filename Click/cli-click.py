a02fb1cd7771e051812ffe954813c51b240807d0
Yongtae Park <samuel.park@megazone.com>
2023-05-28 16:05:40 +0900

    feat: basic practice

---
+++ Click/cli-click.py
@@ -0,0 +1,12 @@
+import click
+
+
+@click.command()
+@click.argument('name')
+@click.option('--greeting', '-g')
+def main(name, greeting):
+    click.echo('{}, {}'.format(greeting, name))
+
+
+if __name__ == '__main__':
+    main()
